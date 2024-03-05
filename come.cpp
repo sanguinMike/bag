9db0a49647d2a2b4fc85d256e3112160c5e8ae6e
Michael Oscar <162304988+sanguinMike@users.noreply.github.com>
2024-03-05 12:06:59 +0200

    Create come.cpp

---
+++ come.cpp
@@ -0,0 +1,25 @@
+  } else if(keyword == "v") {
+            ++positionIndexOffset;
+            thisIsFirstMeshAndItHasNoData = false;
+        } else if(keyword == "vt") {
+            ++textureCoordinateIndexOffset;
+            thisIsFirstMeshAndItHasNoData = false;
+        } else if(keyword == "vn") {
+            ++normalIndexOffset;
+            thisIsFirstMeshAndItHasNoData = false;
+
+UnsignedInt ObjImporter::doMeshCount() const { return _file->meshes.size(); }
+
+Int ObjImporter::doMeshForName(const Containers::StringView name) {
+    const auto it = _file->meshesForName.find(name);
+    return it == _file->meshesForName.end() ? -1 : it->second;
+}
+
+Containers::String ObjImporter::doMeshName(UnsignedInt id) {
+    return _file->meshNames[id];
+}
+
+     const std::size_t keywordEnd = line.find(' ');
+        const std::string keyword = line.substr(0, keywordEnd);
+        const std::string contents = keywordEnd != std::string::npos ?
+            Utility::String::ltrim(line.substr(keywordEnd+1)) : "";
